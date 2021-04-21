6655446a3ef234f4f6dc410fffb404b50c532e94
Rojina Thing <rojina2415@gmail.com>
2021-04-21 20:08:21 +0545

    RT: completed courosol

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+new Glider(document.querySelector(".testimonials"),{
+    slidesToShow: 1,
+    dots: ".dots",
+     arrows:{
+       prev: '.slider-prev',
+       next: '.slider-next',
+     },
+  });
